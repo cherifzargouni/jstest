dfadfb55c4450269bfddafdc5454c8c7a38c537b
chrif <chris@chrif>
2021-11-26 16:19:45 +0100

    HAHAHAHAHA

--- util/path.js
+++ util/path.js
@@ -2,4 +2,7 @@ const path = require('path');
 
 module.exports = path.dirname(process.mainModule.filename);
 // làol
-//
+//git tag version0.1 -m"version sans mise en page"
+
+
+git tag version0.1 -m"version sans mise en page"
